e8116a87ba3888d39e1646a2afe9f6994093658a
suisho <suisho000000@gmail.com>
2013-01-31 02:53:38 +0900

    some index.js code move to mirror.js

--- index.js
+++ index.js
@@ -1,49 +1,21 @@
-var mirror = require("./mirror")
-var Github = require('github');
-var github = new Github({
+var async = require("async");
+var github = require('github')({
   version : "3.0.0",
 });
 
-var async = require("async");
-
-var mirrorUser = function(user, cb){
-  var param = {
-    user : user
-  }
-  github.repos.getFromUser(param, function(err, repos){
-    mirrorRepos(repos, cb);
-  });
-};
-
-var mirrorRepos = function(repos, cb){
-  var tasks = [];
-  repos.forEach(function(repo){
-    tasks.push(function(next){
-      console.log("start  " + repo.full_name+ " mirroring");
-      mirror(mirrorDir, repo, function(err){
-        if(err) console.log(err);
-        console.log("finish " + repo.full_name+ " mirroring");
-        next();
-      })
-    })
-  })
-  async.series(tasks, function(err, result){
-    cb(err, result)
-  });
-}
-
 
 var users = ["suisho", "gruntjs"]
+var ignorePattern = "";
 var mirrorDir = "./test2"
+var mirror = require("./mirror")(github, mirrorDir,{});
 var userTask = [];
 
 users.forEach(function(user){
   userTask.push(function(next){
-    mirrorUser(user, function(){
+    api.mirror(user, {}, function(){
       next()
     })
   })
-  
 })
 async.series(userTask, function(){
   console.log("finished");
--- mirror.js
+++ mirror.js
@@ -1,29 +1,81 @@
 var exec = require('child_process').exec
+var spawn = require('child_process').spawn
 var fs = require("fs")
 var path = require("path")
 var mkdirp = require("mkdirp")
-// clone --bare --mirror to /baseDir/owner/project or fetch
-module.exports = function(baseDir, repoInfo, cb){
-  var project = repoInfo.full_name
-  var projectDir = path.resolve(path.join(baseDir, project))
-  var bareProjectDir = projectDir+".git";
-  if(fs.existsSync(bareProjectDir)){
-    fetch(bareProjectDir, cb);
-    return;
+var async = require("async");
+module.exports = function(github, mirrorDir, options){
+  // mirror all repos(series)
+  var mirrorRepos = function(repos, cb){
+    var tasks = [];
+    repos.forEach(function(repo){
+      tasks.push(function(next){
+        mirror(repo, function(err){
+          next();
+        })
+      })
+    })
+    async.series(tasks, function(err, result){
+      cb(err, result)
+    });
   }
   
-  var ownerDir = path.dirname(projectDir);
-  if(!fs.existsSync(ownerDir)){
-    mkdirp.sync(ownerDir)
+  // mirror repo
+  var mirror = function(repo, cb){
+    console.log("[mirror]start  " + repo.full_name+ " mirroring");
+    var project = repo.full_name
+    var projectDir = path.resolve(path.join(mirrorDir, project))
+    var bareProjectDir = projectDir + ".git";
+    if(fs.existsSync(bareProjectDir)){
+      fetch(bareProjectDir, cb);
+      return;
+    }
+    
+    var ownerDir = path.dirname(projectDir);
+    if(!fs.existsSync(ownerDir)){
+      mkdirp.sync(ownerDir)
+    }
+    var url = options.use_git_url ? repo.git_url : repo.ssh_url;
+    clone(ownerDir, repo.git_url, cb);
   }
-  clone(ownerDir, repoInfo.git_url, cb);
+
+  return {
+    user : function(user, option, cb){
+      var param = {
+        user : user
+      }
+      if(typeof cb != "function"){
+        cb = function(){}
+      }
+      github.repos.getFromUser(param, function(err, repos){
+        mirrorRepos(repos, cb);
+      });
+    },
+  }
 }
 
+var command = function(command, option, cb){
+  
+  console.log(command);
+  var _command = command.split(" ")
+  var _c = _command.shift();
+  var args = _command.concat();
+  var cmd = spawn(_c, args, option);
+  cmd.stdout.on('data',function(data){
+    console.log(data.toString());
+  });
+  cmd.stderr.on('data',function(data){
+    console.error(data.toString());
+  });
+  cmd.on('exit',function(code){
+    cb(null, code);
+  })
+}
 var fetch = function(dir, cb){
   var opt = {
     "cwd" : dir
   }
-  exec("git fetch --all", opt, function(err, out){
+  command("git fetch --all", opt, function(err, code){
     cb(err)
   })
 }
@@ -31,7 +83,7 @@ var clone = function(dir, url, cb){
   var opt = {
     "cwd" : dir
   }
-  exec("git clone --mirror --bare " + url, opt, function(err,out){
+  command("git clone --mirror --bare " + url, opt, function(err, code){
     cb(err)
   })
 }
